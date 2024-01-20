b8ce90cea6e5995d9ec43a19ad696b95957a2742
medinajorge <medinahdezjorge@gmail.com>
2024-01-21 09:24:33 +1100

    cleaned

--- scripts/Grib_pruner.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-for year in 1985 #$(seq 1985 1986)
-do run -c 1 -m 30 -o stdin_files/grib_pruner_"$year".out -e stdin_files/grib_pruner_"$year".err "julia Grib_pruner.jl -y $year"
-done
